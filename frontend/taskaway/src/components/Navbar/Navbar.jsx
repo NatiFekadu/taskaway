5e72618b6f21231403a126d602a07ce66e6eb2ba
NatiFekadu <Natifekadu808@gmail.com>
2024-05-20 14:06:19 +0300

    Profile Info

--- frontend/taskaway/src/components/Navbar/Navbar.jsx
+++ frontend/taskaway/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import ProfileInfo from "../Cards/ProfileInfo";
+
 const Navbar = () => {
+  const navigate = useNavigate();
+  const onLogout = () => {
+    navigate("/login");
+  };
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2 ">Taskaway</h2>
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
--- frontend/taskaway/src/utils/helper.js
+++ frontend/taskaway/src/utils/helper.js
@@ -6,3 +6,14 @@ export const validateEmail =(email) =>{
 //   const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\s]).{8,}$/;
 //   return regex.test(password);
 // };
+
+export const getInitials =(name)=>{
+    if(!name) return "";
+    const words = name.split(" ");
+    let initails ="";
+       for (let  i=0; i< Math.min(words.length,2);i++){
+                 initails += words[i][0];
+       }
+
+       return initails.toUpperCase();
+}
