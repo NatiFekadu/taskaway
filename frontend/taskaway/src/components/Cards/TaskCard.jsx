7c9668decaf9bb43d4c2291ade32f0cb4804bab9
NatiFekadu <Natifekadu808@gmail.com>
2024-05-20 14:57:31 +0300

    TaskCard

---
+++ frontend/taskaway/src/components/Cards/TaskCard.jsx
@@ -0,0 +1,25 @@
+import { MdCreate, MdDelete } from "react-icons/md";
+const TaskCard = ({ title, date, onEdit, onDelete }) => {
+  return (
+    <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
+      <div className="flex items-center justify-between">
+        <div>
+          <h6 className="text-sm font-medium">{title}</h6>
+          <span className="text-xs text-slate-500">{date}</span>
+        </div>
+        <div className="flex items-center gap-3">
+          <MdCreate
+            className=" icon-btn hover:text-green-600"
+            onClick={onEdit}
+          />
+          <MdDelete
+            className="icon-btn hover:text-red-600"
+            onClick={onDelete}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
--- frontend/taskaway/src/pages/Home/Home.jsx
+++ frontend/taskaway/src/pages/Home/Home.jsx
@@ -1,9 +1,51 @@
+import { MdAdd } from "react-icons/md";
+import TaskCard from "../../components/Cards/TaskCard";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div className=" container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <TaskCard
+            title="Go to the Jim"
+            date="7th aprill"
+            onEdit={() => {}}
+            onDelete={() => {}}
+          />
+          <TaskCard
+            title="Go to the Jim"
+            date="7th aprill"
+            onEdit={() => {}}
+            onDelete={() => {}}
+          />
+          <TaskCard
+            title="Go to the Jim"
+            date="7th aprill"
+            onEdit={() => {}}
+            onDelete={() => {}}
+          />
+          <TaskCard
+            title="Go to the Jim"
+            date="7th aprill"
+            onEdit={() => {}}
+            onDelete={() => {}}
+          />
+          <TaskCard
+            title="Go to the Jim"
+            date="7th aprill"
+            onEdit={() => {}}
+            onDelete={() => {}}
+          />
+        </div>
+      </div>
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute  right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
     </>
   );
 };
