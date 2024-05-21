b3b48d4f21b79c94d36051fb881d3398c02e33f7
NatiFekadu <Natifekadu808@gmail.com>
2024-05-21 09:04:50 +0300

    Login

--- backend/index.js
+++ backend/index.js
@@ -31,14 +31,18 @@ app.post("/create-account", async (req, res) => {
   const { fullname, email, password } = req.body;
 
   if (!fullname) {
-    return res.status(400).json({ error:true,message: "fullname is required" });
+    return res
+      .status(400)
+      .json({ error: true, message: "fullname is required" });
   }
   if (!email) {
-    return res.status(400).json({ error:true,message: "email is required" });
+    return res.status(400).json({ error: true, message: "email is required" });
   }
 
   if (!password) {
-    return res.status(400).json({ error:true,meessage: "password is required" });
+    return res
+      .status(400)
+      .json({ error: true, meessage: "password is required" });
   }
 
   const isUser = await User.findOne({ email: email });
@@ -53,19 +57,61 @@ app.post("/create-account", async (req, res) => {
   });
 
   await user.save();
-  const accessToken = jwt.sign({
-    user
-  },process.env.ACCESS_TOKEN_SECRET,{
-    expiresIn: "60m"
-  });
+  const accessToken = jwt.sign(
+    {
+      user,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "60m",
+    }
+  );
 
   return res.json({
-       error:false,
-       user,
-       accessToken,
-       message:"Registration Successful"
-  })
+    error: false,
+    user,
+    accessToken,
+    message: "Registration Successful",
+  });
+});
 
+//Login
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: true, message: "email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "password is required" });
+  }
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res
+      .status(400)
+      .json({ error: true, message: "User does not exist" });
+  }
+
+  if (userInfo.email === email && userInfo.password === password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "60m",
+    });
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+      
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ error: true, message: "Invalid Credentials" });
+  }
 });
 
 app.listen(8000);
